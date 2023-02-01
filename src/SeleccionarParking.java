83a20e778535db5795500172e291979fd5d214ff
gorkade <57218921+gorkade@users.noreply.github.com>
2023-02-01 09:02:49 +0100

    He conectado las pantallas que tenemos

--- src/SeleccionarParking.java
+++ src/SeleccionarParking.java
@@ -13,19 +13,9 @@ public class SeleccionarParking extends JFrame implements ActionListener {
     private JMenuItem menuReserva, menuHorarios,menuDatos;
 
     private JScrollBar scrollBar;
-    private JSpinner numCamas;
-    /*items del menu Tipo*/
-    String[] tiposHabitacion = {null , "Sencilla", "Doble", "Matrimonial", "Suite"};
-    String[] tiposParking = {null, "Coche (+10€/noche)", "Moto (+5€/noche)"};
-
-    private JComboBox comboTipo, comboTipoParking;
-    private JDateChooser calendarioLlegada;
-    private JDateChooser calendarioSalida;
 
     /*labels de los menus para mostrar en pantalla*/
-    private JLabel titulo, labelTipo, labelNumCamas, labelLlegada, labelSalida, labelTipoParking;
-
-    private JCheckBox reservaParking;
+    private JLabel titulo;
 
     private Button buscar;
 
@@ -33,7 +23,7 @@ public class SeleccionarParking extends JFrame implements ActionListener {
     {
         iniciarComponentes();
         //Asigna un titulo a la barra de titulo
-        setTitle("Menú Hotel : Reserva");
+        setTitle("Menú Hotel : Reserva Parking");
         //tamaño de la ventana
         setSize(700,330);
         //pone la ventana en el Centro de la pantalla
@@ -55,26 +45,9 @@ public class SeleccionarParking extends JFrame implements ActionListener {
         menuHorarios = new JMenuItem("Horarios");
         menuDatos = new JMenuItem("Datos");
         barraMenu = new JMenuBar();
-        comboTipo = new JComboBox(tiposHabitacion);
         scrollBar = new JScrollBar();
 
-        calendarioLlegada= new JDateChooser();
-        calendarioLlegada.setMinSelectableDate(new java.util.Date());
-        calendarioSalida= new JDateChooser();
-
-        reservaParking = new JCheckBox("Reserva Parking");
-        comboTipoParking = new JComboBox(tiposParking);
-
-        numCamas = new JSpinner();
-
         titulo = new JLabel();
-        labelTipo = new JLabel();
-        labelNumCamas = new JLabel();
-
-        labelLlegada =new JLabel();
-        labelSalida =new JLabel();
-
-        labelTipoParking =new JLabel();
 
         buscar = new Button("Buscar");
 
@@ -83,33 +56,7 @@ public class SeleccionarParking extends JFrame implements ActionListener {
 
         /*Labels*/
         titulo.setBounds(10,0,200,30);
-        titulo.setText("RESERVA DE HABITACIONES");
-
-        labelTipo.setBounds(10,20,130,30);
-        labelTipo.setText("Tipo de habitación : ");
-
-        comboTipo.setBounds(135,22,150,20);
-
-        labelNumCamas.setBounds(10,40,150,30);
-        labelNumCamas.setText("Número de Camas: ");
-
-        numCamas.setBounds(135,42,150,20);
-
-        labelLlegada.setBounds(10,95,100,50);
-        labelLlegada.setText("Fecha Llegada : ");
-
-        labelSalida.setBounds(350,95,100,50);
-        labelSalida.setText("Fecha Salida : ");
-
-        calendarioLlegada.setBounds(115,105,130,30);
-        calendarioSalida.setBounds(455,105,130,30);
-
-        reservaParking.setBounds(10, 170, 150, 30);
-
-        labelTipoParking.setBounds(10,180,100,50);
-        labelTipoParking.setText("Tipo Parking : ");
-
-        comboTipoParking.setBounds(115,200,150,20);
+        titulo.setText("RESERVA PLAZA DE PARKING:");
 
         buscar.setBounds(300, 200, 100, 30);
 
@@ -125,48 +72,19 @@ public class SeleccionarParking extends JFrame implements ActionListener {
 
         setJMenuBar(barraMenu);
 
-        calendarioSalida.setDateFormatString("dd/MM/yyyy");
-        calendarioLlegada.setDateFormatString("dd/MM/yyyy");
-
         menuHorarios.addActionListener(this);
         menuDatos.addActionListener(this);
-        reservaParking.addActionListener(this);
         buscar.addActionListener(this);
-        calendarioLlegada.getDateEditor().addPropertyChangeListener(
-                evt -> calendarioSalida.setMinSelectableDate(calendarioLlegada.getDate()));
 
         miPanel.add(titulo);
-        miPanel.add(labelTipo);
-        miPanel.add(labelNumCamas);
-        miPanel.add(numCamas);
-        miPanel.add(comboTipo);
-        miPanel.add(labelLlegada);
-        miPanel.add(labelSalida);
-        miPanel.add(calendarioLlegada);
-        miPanel.add(calendarioSalida);
-        miPanel.add(reservaParking);
-        miPanel.add(labelTipoParking);
-        miPanel.add(comboTipoParking);
         miPanel.add(buscar);
         add(miPanel);
 
         buscar.setVisible(true);
-        labelTipoParking.setVisible(false);
-        comboTipoParking.setVisible(false);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==reservaParking){
-            if(reservaParking.isSelected()){
-                labelTipoParking.setVisible(true);
-                comboTipoParking.setVisible(true);
-            }else{
-                labelTipoParking.setVisible(false);
-                comboTipoParking.setVisible(false);
-                comboTipoParking.setSelectedIndex(0);
-            }
-        }
 
         if(e.getSource()==menuHorarios){
 
@@ -186,21 +104,8 @@ public class SeleccionarParking extends JFrame implements ActionListener {
         }
 
         if(e.getSource()==buscar){
-            String tipoHabitacion = (String) comboTipo.getSelectedItem();
-            int numCamasHabitacion = (int) numCamas.getValue();
-            String fechaLlegada = calendarioLlegada.getDate().toString();
-            String fechaSalida = calendarioSalida.getDate().toString();
-            String tipoParking = (String) comboTipoParking.getSelectedItem();
-
-            HabitacionesDisponibles ventanaHabitaciones = null;
-            try {
-                ventanaHabitaciones = new HabitacionesDisponibles(tipoHabitacion,numCamasHabitacion,fechaLlegada,fechaSalida, tipoParking);
-            } catch (SQLException ex) {
-                JOptionPane.showMessageDialog(null,"Error con la base de datos");
-            }
-
-            ventanaHabitaciones.setVisible(true);
-
+            DatosReserva datosReserva = new DatosReserva();
+            datosReserva.setVisible(true);
             this.setVisible(false);
         }
     }
