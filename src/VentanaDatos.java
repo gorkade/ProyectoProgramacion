2e8689faea7c116058fcc536ad5842dd45c7d6f6
gorkade <57218921+gorkade@users.noreply.github.com>
2023-02-21 11:12:09 +0100

    VentanaDatos comentada

--- src/VentanaDatos.java
+++ src/VentanaDatos.java
@@ -8,7 +8,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class VentanaDatos extends JFrame implements ActionListener {
-    private JPanel miPanel;//contenedor de los componentes
+    //Esta clase muestra la ventana de datos de las personas
+
+    //Atributos
+    private JPanel miPanel;
     private JMenuBar barraMenu;
     private JMenuItem menuReserva, menuHorarios,menuDatos;
 
@@ -22,6 +25,7 @@ public class VentanaDatos extends JFrame implements ActionListener {
 
     public VentanaDatos()//constructor
     {
+        //llamamos a método para crear la ventana y creamos el panel con sus componentes y la configuración posterior
         iniciarComponentes();
         //Asigna un titulo a la barra de titulo
         setTitle("Menú Hotel : Consultar Datos");
@@ -36,8 +40,6 @@ public class VentanaDatos extends JFrame implements ActionListener {
     }
 
     private void iniciarComponentes() {
-
-        /**/
 
         /*Inicia instancias de los componentes*/
         miPanel = new JPanel();
@@ -59,7 +61,7 @@ public class VentanaDatos extends JFrame implements ActionListener {
 
         consultar = new Button("Consultar");
 
-        /*Labels*/
+        //Configuramos la posicion y los textos de los componentes
         titulo.setBounds(10,0,250,30);
         titulo.setText("DATOS PERSONAS:");
 
@@ -92,21 +94,29 @@ public class VentanaDatos extends JFrame implements ActionListener {
         menuDatos.setText("Consultar Datos Cliente/Empleado");
         barraMenu.add(menuDatos);
 
+        //Añadimos la barra del menu a la ventana
         setJMenuBar(barraMenu);
 
+        //Añadimos los Listener a los botones
         consultar.addActionListener(this);
         menuReserva.addActionListener(this);
         menuHorarios.addActionListener(this);
 
+        //programamos el boton añadir para añadir empleados a partir de un archivo
         añadir.addActionListener(p -> {
+            //Creamos un objeto File para abrir el archivo
             File archivo = new File("empleados.txt");
             try {
+                //Creamos un objeto FileReader y BufferedReader para leer el archivo
                 FileReader fr = new FileReader(archivo);
                 BufferedReader br = new BufferedReader(fr);
                 String linea;
 
+                //leemos el archivo linea a linea
                 while ((linea = br.readLine()) != null) {
+                    //separamos los datos de cada linea por el caracter ";" y los guardamos en un array
                     String[] datos = linea.split(";");
+                    //Gurdamos todos los datos en variables
                     String DNI = datos[0];
                     String NumEmpleado = datos[1];
                     String Nombre = datos[2];
@@ -117,10 +127,13 @@ public class VentanaDatos extends JFrame implements ActionListener {
                     String TipoEmpleado = datos[7];
                     String Salario = datos[8];
 
+                    //Nos conectamos a la base de datos
                     Statement st = ConexionDB.miConexion.createStatement();
 
+                    //Con esta consulta comprobamos si el empleado ya existe en la base de datos
                     ResultSet rs = st.executeQuery("SELECT * FROM Empleado WHERE DNI = '" + DNI + "'");
 
+                    //Si el empleado no existe en la base de datos, lo añadimos
                     if (!rs.next()) {
                         st.executeUpdate("INSERT INTO Empleado VALUES ('" + DNI + "','" + NumEmpleado + "','" + Nombre + "','" + Apellido + "','" + HoraInicio + "','" + HoraFinal + "','" + Zona + "','" + TipoEmpleado + "','" + Salario + "')");
                     }
@@ -136,6 +149,7 @@ public class VentanaDatos extends JFrame implements ActionListener {
             }
         });
 
+        //Añadimos los componentes al panel
         miPanel.add(titulo);
         miPanel.add(labelTipoPersona);
         miPanel.add(comboTipoPersona);
@@ -153,14 +167,20 @@ public class VentanaDatos extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        //programamos el boton consultar
         if (e.getSource() == consultar) {
             Statement miStatement = null;
+            //Recogemos el DNI de la persona que queremos consultar
             String DNI =  dni.getText();
 
+            //Si es un empleado
             if (comboTipoPersona.getSelectedItem().equals("Empleado")) {
                    try {
+                       //Nos conectamos a la BD y consultamos todos los datos del empleado
                         miStatement = ConexionDB.miConexion.createStatement();
                        ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM Empleado where DNI like '" + DNI + "'");
+
+                       //Mostramos los resultados si dicho empleado existe en la base de datos
                        if (miResultSet.next()) {
                            labelDatos.setText("Datos: ");
                            labelDatos.setText("NumEpleado: " + miResultSet.getString("NumEmpleado") +
@@ -170,15 +190,21 @@ public class VentanaDatos extends JFrame implements ActionListener {
                            labelDatos2.setText("Salario: " + miResultSet.getString("Salario")  );
 
                        }else{
+                           //Si no existe, mostramos un mensaje de error
                            JOptionPane.showMessageDialog(null,"No se ha encontrado el Empleado");
                        }
                    } catch (SQLException ex) {
+                       //SI hay algun error en la conexion, mostramos un mensaje de error
                        JOptionPane.showMessageDialog(null,"Error al buscar los datos");
                    }
+                   //Si es un cliente
                }else if (comboTipoPersona.getSelectedItem().equals("Cliente")) {
                 try {
+                    //Nos conectamos a la BD y consultamos todos los datos del empleado
                     miStatement = ConexionDB.miConexion.createStatement();
                     ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM Cliente where DNI like '"+DNI+"'");
+
+                    //Mostramos los resultados si dicho cliente existe en la base de datos
                     if(miResultSet.next()) {
                         labelDatos.setText("Datos: ");
                         labelDatos.setText("Nombre: " + miResultSet.getString("Nombre") + " Aplleidos: " + miResultSet.getString("Apellido") +
@@ -187,16 +213,20 @@ public class VentanaDatos extends JFrame implements ActionListener {
                         labelDatos2.setText("Dirección: " + miResultSet.getString("Direccion") + " Pais: " + miResultSet.getString("Pais") +
                                 " Ciudad: " + miResultSet.getString("Ciudad") + " CP: " + miResultSet.getString("CP") );
                     }else{
+                        //Si no existe el cliente, mostramos un mensaje de error
                         JOptionPane.showMessageDialog(null,"No se ha encontrado el cliente");
                     }
                 } catch (SQLException ex) {
+                    //SI hay algun error en la conexion, mostramos un mensaje de error
                     JOptionPane.showMessageDialog(null,"Error al buscar los datos");
                 }
             }
+            //Hacemos visible los labels donde se muestran los datos
             labelDatos.setVisible(true);
         }
 
         if (e.getSource() == menuReserva) {
+            //Abrimos la ventana de inicial de reserva
             InicioReserva miFrame= new InicioReserva();
             miFrame.setVisible(true);
             this.setVisible(false);
@@ -204,6 +234,7 @@ public class VentanaDatos extends JFrame implements ActionListener {
 
         if (e.getSource() == menuHorarios) {
 
+            //Abrimos la ventana de horarios
                 VentanaHorarios miFrame = new VentanaHorarios();
 
                 miFrame.setVisible(true);
